dcf9df1bd09b00fe7d16df23ec9c7ea2cb0d1f18
Animesh Kumar <kumaranimesh097@gmail.com>
2023-03-31 18:47:07 +0530

    removed react cdn links and installed react as a package via npm

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {}, "hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); //
+ // continue lecture 2, at 1:04:00
+
+//# sourceMappingURL=index.6bd02f5a.js.map
