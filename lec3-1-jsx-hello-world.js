504c9302e571c223f004cb28ffa080faddbc8191
Animesh Kumar <kumaranimesh097@gmail.com>
2023-04-01 21:59:52 +0530

    hello world using JSX

---
+++ lec3-1-jsx-hello-world.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX - html-like syntax or xml-like syntax
+const jsxHeading = <h1>hello world using JSX</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
