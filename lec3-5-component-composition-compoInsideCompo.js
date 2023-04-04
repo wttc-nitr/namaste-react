aaf7441f599264a64cdd2a01c857d72ed91f1117
Animesh Kumar <kumaranimesh097@gmail.com>
2023-04-04 11:52:46 +0530

    three ways to inject a functional component inside another functional component

--- lec3-5-component-composition-compoInsideCompo.js
+++ lec3-5-component-composition-compoInsideCompo.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const C1 = () => (
-  <h1>hello world</h1>
-);
+const C1 = () => <h1>hello world</h1>;
 
 const C2 = () => (
   <div>
-    <C1/>
+    <C1 />
     <h2>using component-composition</h2>
   </div>
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// another way to inject component inside another component
+const C3 = () => (
+  <div>
+    {C1()}
+    <C1 />
+    <C1></C1>
+    <h2>another way to inject a component inside another component.</h2>
+  </div>
+);
 
-root.render(<C2/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<C3 />);
