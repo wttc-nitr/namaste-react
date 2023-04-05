4e21281b6f9382c4262b60e35fa5dac60f8bcea0
Animesh Kumar <kumaranimesh097@gmail.com>
2023-04-05 10:55:13 +0530

    destructuring in JS

---
+++ lec4-3-destructuring.js
@@ -0,0 +1,13 @@
+const person = {
+  Name: "animesh",
+  Age: 26,
+};
+
+/**
+ * console.log(person.Name, person.Age);
+ */
+
+const { name, age } = person;
+
+// now you can use 'name' and 'age' directly
+console.log(Name, Age);
